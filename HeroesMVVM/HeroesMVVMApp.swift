d6f9c0cdd78fa8401b800aa867370c74a5566a7d
drinconvega <78913998+drinconvega@users.noreply.github.com>
2022-01-14 12:43:19 +0100

    Initial Commit

---
+++ HeroesMVVM/HeroesMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesMVVMApp.swift
+//  HeroesMVVM
+//
+//  Created by Daniel Rincon Vega on 14/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HeroesMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
